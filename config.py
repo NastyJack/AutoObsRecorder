bf0ee123aa6ab34eba8bec55fc61809c2a33adae
NastyJack <cross99.rishabh@gmail.com>
2025-11-16 19:18:02 +0530

    updated exe name in config list for The Finals

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 
-gameList = ["RocketLeague.exe", "r5apex.exe", "eldenring.xe", "noita.exe", "InsurgencyClient-Win64-Shipping.exe",
-            "Discovery-e.exe", "PrinceOfPersia.exe", "FSD-Win64-Shipping.exe", "ReadyOrNot.exe", "XDefiant.exe", "GTFO.exe"]
+gameList = [
+    "RocketLeague.exe",
+    "r5apex.exe",
+    "eldenring.xe",
+    "noita.exe",
+    # "InsurgencyClient-Win64-Shipping.exe",
+    "Discovery-d.exe",
+    "PrinceOfPersia.exe",
+    "FSD-Win64-Shipping.exe",
+    "ReadyOrNot.exe",
+    "PioneerGame.exe"]
